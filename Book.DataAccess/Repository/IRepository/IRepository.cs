9f2736c6d4d98fce957b5eb3df03770519a4f487
fardin0045 <fardinonik40@gmail.com>
2026-02-16 19:51:19 +0600

    Repository

---
+++ Book.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Book.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        // T - Category  or any other generic model we want to
+        //perform CRUD operations 
+
+        //Suppose T - Category
+        IEnumerable<T> GetAll();
+
+        //Link operator u=> u.id ==id
+        T Get(Expression<Func<T, bool>> filter);
+
+        //Add Method 
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange (IEnumerable<T> entity);
+
+    }
+}
