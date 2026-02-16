d6e377e195fdbb85864e6db36fbc1dccb10d20f6
fardin0045 <fardinonik40@gmail.com>
2026-02-16 20:57:21 +0600

    register the app

--- BookWeb/Program.cs
+++ BookWeb/Program.cs
@@ -1,5 +1,6 @@
 using Book.DataAccess.Data;
-
+using Book.DataAccess.Repository;
+using Book.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Icategory Registration
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
