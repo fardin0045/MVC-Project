efea567e52b34c1c3632033a308b29f246570d54
fardin0045 <fardinonik40@gmail.com>
2026-03-10 19:55:13 +0600

    login and registration

--- BookWeb/Program.cs
+++ BookWeb/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
 //Rajor pages 
 builder.Services.AddRazorPages();
 //Icategory Registration
